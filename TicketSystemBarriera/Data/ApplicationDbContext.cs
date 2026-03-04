fbc02093f5067c6a4b4509ce20f6f9218047ed97
ALEXIS Y BARRIERA-PACHECO <alexis.barriera@upr.edu>
2026-03-04 16:28:45 -0400

    Register confirmation and database

--- TicketSystemBarriera/Data/ApplicationDbContext.cs
+++ TicketSystemBarriera/Data/ApplicationDbContext.cs
@@ -1,9 +1,8 @@
+
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
-
-namespace TicketSystemBarriera.Data
+namespace TicketSystemBarriera.Data;
+ public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser>(options)
-    {
-    }
+    // Aquí registraremos los DbSets en el Módulo 3
 }
